7ddf157d8e5a1d451cf5c78ef1e419720043f4ec
vietpt21 <vietvpvbhp@gmail.com>
2024-07-12 08:13:07 +0700

    Type

---
+++ dist/2-number-string-booleanType.js
@@ -0,0 +1,5 @@
+"use strict";
+let price = 33;
+let cournName = "Join";
+let kt = true;
+console.log(price, cournName, kt);
---
+++ src/2-number-string-booleanType.ts
@@ -0,0 +1,6 @@
+
+let price:number =33;
+let cournName:string ="Join";
+let kt :boolean=true; 
+console.log(price,cournName,kt);
+
